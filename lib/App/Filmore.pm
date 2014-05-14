4e688e8b1d2f6731942d3a34d1f3f95bc431e531
Bernie Simon <bernie.simon@gmail.com>
2014-05-14 09:13:09 -0400

    Initial Version

---
+++ lib/App/Filmore.pm
@@ -0,0 +1,39 @@
+package App::Filmore;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::Filmore - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::Filmore;
+
+=head1 DESCRIPTION
+
+App::Filmore is ...
+
+=head1 LICENSE
+
+Copyright (C) Bernie Simon.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Bernie Simon E<lt>bernie.simon@gmail.comE<gt>
+
+=cut
+
