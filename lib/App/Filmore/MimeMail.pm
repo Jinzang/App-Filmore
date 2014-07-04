a22d4aa79cc0a7e04d1f8e7ea21d23caf89b8673
Bernie Simon <bernie.simon@gmail.com>
2014-07-04 14:53:29 -0400

    Code for mime mile message handling

---
+++ lib/App/Filmore/MimeMail.pm
@@ -0,0 +1,42 @@
+use strict;
+use warnings;
+
+#----------------------------------------------------------------------
+# Wrapper for MIME_Mail
+
+package App::Filmore::MimeMail;
+
+use lib '../../../lib';
+use base qw(App::Filmore::ConfiguredObject);
+
+our $VERSION = '0.01';
+
+use App::Filmore::MIME_Lite;
+
+#----------------------------------------------------------------------
+# Set the default parameter values
+
+sub parameters {
+    my ($pkg) = @_;
+    
+    return (
+        method => 'sendmail',
+        attachment_type => 'text/html',
+    );
+}
+
+#----------------------------------------------------------------------
+# Send a mail message with optional attachment
+
+sub send_mail {
+    my ($self, $mail_fields, $msg, $attachment) = @_;
+
+    my %mail_fields = (%$mail_fields, 'Type' => 'multipart/mixed');
+    my $lite = MIME::Lite->new(%mail_fields);
+
+    $lite->attach(Type =>'TEXT', Data => $msg);  
+    $lite->attach(Type => $self->{attachment_type}, Data => $attachment);
+    $lite->send($self->{method});
+    
+    return;
+}
